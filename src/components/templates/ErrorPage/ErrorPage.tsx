60505b65cc55666c4e219dcf9d54474ffa0e5e2e
DaanKorver <daank2000@hotmail.nl>
2023-05-25 16:28:33 +0200

    Fix for vercel

---
+++ src/components/templates/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,7 @@
+import styles from './ErrorPage.module.css'
+
+interface ErrorPageProps {}
+
+export function ErrorPage(props: ErrorPageProps) {
+	return <h1>Error</h1>
+}
---
+++ src/pages/_error.tsx
@@ -0,0 +1,30 @@
+import { ErrorPage } from '@/components/templates/ErrorPage/ErrorPage'
+import { GetStaticProps, NextPage } from 'next'
+import { nextClient } from '../../lib/client'
+import { GetLayoutQuery } from '../../lib/generated/sdk'
+
+export interface ErrorProps {
+	statusCode?: number | null
+}
+
+const Error: NextPage<ErrorProps> = props => <ErrorPage {...props} />
+
+export const getStaticProps: GetStaticProps = async () => {
+	try {
+		const layoutQuery = nextClient.getLayout()
+
+		const [layout] = await Promise.allSettled([layoutQuery])
+
+		return {
+			props: {
+				layout: (layout as PromiseFulfilledResult<GetLayoutQuery>).value,
+			},
+		}
+	} catch (error) {
+		return {
+			notFound: true,
+		}
+	}
+}
+
+export default Error
