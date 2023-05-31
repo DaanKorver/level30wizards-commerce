92718e15063d68918531225dd8063201fb945d6e
DaanKorver <daank2000@hotmail.nl>
2023-05-31 17:07:13 +0200

    Removed G.I.P.

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -27,9 +27,4 @@ export const getStaticProps: GetStaticProps = async () => {
 	}
 }
 
-Error.getInitialProps = ({ res, err }) => {
-	const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-	return { statusCode }
-}
-
 export default Error
